67acb1b0a40a6ae2b991d98e3041fd09ca28811f
Kaviya-Samandhapathiran <kaviyajain0030@gmail.com>
2025-02-26 20:52:44 +0530

    changes

--- index.js
+++ index.js
@@ -9,21 +9,21 @@ const app = express()
 app.use(cors(corsOptions))
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://bulkmail-frontend-beta.vercel.app/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://bulkmail-frontend-beta.vercel.app"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
   
   app.options('*', (req, res) => { 
     // Pre-flight request. Reply successfully:
-    res.header('Access-Control-Allow-Origin', 'https://bulkmail-frontend-beta.vercel.app/');
+    res.header('Access-Control-Allow-Origin', 'https://bulkmail-frontend-beta.vercel.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.send();
   });
   
 var corsOptions = {
-    origin: ["https://bulkmail-frontend-beta.vercel.app/"]
+    origin: ["https://bulkmail-frontend-beta.vercel.app"]
   };
   
 app.use(express.json())
