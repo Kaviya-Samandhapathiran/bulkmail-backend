0cf6a4d439de0326b66ada6935c6f475d2b5df7b
Kaviya-Samandhapathiran <kaviyajain0030@gmail.com>
2025-02-26 20:49:03 +0530

    cors changes

--- index.js
+++ index.js
@@ -6,7 +6,26 @@ const mongoose = require("mongoose")
 
 const app = express()
 
-app.use(cors())
+app.use(cors(corsOptions))
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://bulkmail-frontend-beta.vercel.app/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
+  app.options('*', (req, res) => { 
+    // Pre-flight request. Reply successfully:
+    res.header('Access-Control-Allow-Origin', 'https://bulkmail-frontend-beta.vercel.app/');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.send();
+  });
+  
+var corsOptions = {
+    origin: ["https://bulkmail-frontend-beta.vercel.app/"]
+  };
+  
 app.use(express.json())
 
 mongoose.connect("mongodb+srv://kaviyajain0030:0310@cluster0.iplds.mongodb.net/passkey?retryWrites=true&w=majority&appName=Cluster0").then(function () {
