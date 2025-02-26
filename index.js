b10f58bcab26ffbd5696e718e1f38965f0b9cbce
Kaviya-Samandhapathiran <kaviyajain0030@gmail.com>
2025-02-26 21:23:18 +0530

    Cors changes

--- index.js
+++ index.js
@@ -6,25 +6,7 @@ const mongoose = require("mongoose")
 
 const app = express()
 
-app.use(cors(corsOptions))
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://bulkmail-frontend-beta.vercel.app"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-  
-  app.options('*', (req, res) => { 
-    // Pre-flight request. Reply successfully:
-    res.header('Access-Control-Allow-Origin', 'https://bulkmail-frontend-beta.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.send();
-  });
-  
-var corsOptions = {
-    origin: ["https://bulkmail-frontend-beta.vercel.app"]
-  };
+app.use(cors())
   
 app.use(express.json())
 
@@ -34,7 +16,7 @@ mongoose.connect("mongodb+srv://kaviyajain0030:0310@cluster0.iplds.mongodb.net/p
     console.log("Failed to connect")
 })
 
-const credential = mongoose.model("credential", {}, "bulkmail")
+const credential = mongoose.model("credential", {}, "bulkmail") 
 
 app.post("/sendmail", function (req, res) {
 
